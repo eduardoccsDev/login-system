1ffd68d19516251a70b0115fa004a040bbbe62d8
eduardoDevSquadra <eduardo.santos@squadra.com.br>
2024-06-18 23:35:18 -0300

    added phpunit and composer

--- public/login.php
+++ public/login.php
@@ -8,7 +8,7 @@
         $erro = null;
 
         if(empty($user) || empty($password)){
-            $erro = "User and password are required"; 
+            $erro = "User and password are required";
         }
 
         //check user match
@@ -32,22 +32,24 @@
 <html lang="en">
 <?php $pageTitle = 'Login'; require '../components/customHead.php'; ?>
 <body>
-    <form action="index.php?router=login" method="post">
-        <h3>Login</h3>
-        <div>
-            <label for="user">User</label>
-            <input type="email" name="user" id="user">
-        </div>
-        <div>
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password">
-        </div>
-        <div>
-            <button type="submit">Login</button>
-        </div>
-    </form>
-    <?php if(!empty($erro)): ?>
-        <p style="color:red"><?= $erro; ?></p>
-    <?php endif ?>
+    <section class="sec-login">
+        <form action="index.php?router=login" method="post">
+            <h3>Login</h3>
+            <div>
+                <label for="user">User:</label>
+                <input type="email" name="user" id="user">
+            </div>
+            <div>
+                <label for="password">Password:</label>
+                <input type="password" name="password" id="password">
+            </div>
+            <div>
+                <button type="submit">Login</button>
+            </div>
+            <?php if(!empty($erro)): ?>
+                <p class="error"><?= $erro; ?></p>
+            <?php endif ?>
+        </form>
+    </section>
 </body>
 </html>
